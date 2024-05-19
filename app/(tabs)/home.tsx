00543bdbf6986b00a1b26f9096822fa3b25b7d12
Faysel Abdella <fayselcode@gmail.com>
2024-05-19 19:04:37 +0300

    - Add categoreis section header

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,12 +1,17 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
 import SearchInput from "@/components/SearchInput";
+import images from "@/constants/images";
+import Category from "@/components/Category";
 
 const Home = () => {
+  const [category, setCatagory] = useState("breakfast");
+
   return (
-    <SafeAreaView className="bg-slate-300">
-      <View className="px-6 py-6">
+    <SafeAreaView className="bg-slate-300 px-6">
+      <View className=" py-6">
         <Text className="text-black-200 font-psemibold text-2xl">
           What are you{" "}
         </Text>
@@ -19,6 +24,49 @@ const Home = () => {
         placeholder="Search any recipe"
         handleChangeText={() => {}}
       />
+
+      <View className="items-center  rounded-3xl">
+        <Image
+          source={images.explor}
+          className="rounded-3xl my-5"
+          resizeMode="contain"
+        />
+      </View>
+
+      <Text className="text-lg font-pbold ">Categories</Text>
+
+      <View className="flex-row justify-between">
+        <Category
+          title="Breakfast"
+          containerStyles={`${
+            category == "breakfast" ? "bg-cyan-500" : "bg-white"
+          }`}
+          textStyles={`${
+            category == "breakfast" ? "text-white" : "text-blaxk-200"
+          }`}
+          handelClick={() => setCatagory("breakfast")}
+        />
+        <Category
+          title="Lunch"
+          containerStyles={`${
+            category == "lunch" ? "bg-cyan-500" : "bg-white"
+          }`}
+          textStyles={`${
+            category == "lunch" ? "text-white" : "text-blaxk-200"
+          }`}
+          handelClick={() => setCatagory("lunch")}
+        />
+        <Category
+          title="Dinner"
+          containerStyles={`${
+            category == "dinner" ? "bg-cyan-500" : "bg-white"
+          }`}
+          textStyles={`${
+            category == "dinner" ? "text-white" : "text-blaxk-200"
+          }`}
+          handelClick={() => setCatagory("dinner")}
+        />
+      </View>
     </SafeAreaView>
   );
 };
